170c0ba9f52be30be5dc0efb8d61f345139054f8
geomatupen <upendra.oli59@gmail.com>
2025-05-31 16:01:04 +0200

    assignment gdal rasterio

---
+++ package/test.py
@@ -0,0 +1,14 @@
+import os
+import sys
+# print(sys.path)
+
+# help("modules")
+# days_of_the_week = 7
+# def say_hello(recipient):
+#     print("hello, world! hello {} ".format(recipient))
+#     return recipient
+
+
+def returnVariable():
+    a = [1,2,3,5,4]
+    return a
--- test.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-print(sys.path)
-
-help("modules")
-days_of_the_week = 7
-def say_hello(recipient):
-    print("hello, world! hello {} ".format(recipient))
-    return recipient
